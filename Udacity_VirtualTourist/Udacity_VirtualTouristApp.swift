0f77867c8dbdd5f29fa23ebb31b99c83c8d8dc7c
VuCuongHD96 <xuancuonghd1102@gmail.com>
2024-05-17 21:56:13 +0700

    Initial Commit

---
+++ Udacity_VirtualTourist/Udacity_VirtualTouristApp.swift
@@ -0,0 +1,17 @@
+//
+//  Udacity_VirtualTouristApp.swift
+//  Udacity_VirtualTourist
+//
+//  Created by Work on 17/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Udacity_VirtualTouristApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
